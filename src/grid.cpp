f7e5ef82bf45e7388afacc4b81c156d3ba710c60
maheem_20 <mwistanbuly@gmail.com>
2023-07-09 23:39:19 +0200

    define colors

--- src/grid.cpp
+++ src/grid.cpp
@@ -35,4 +35,12 @@ void Grid::Print()
 std::vector<Color> Grid::GetCellColors()
 {
     std::vector<Color> colors;
+    Color darkGrey = {26, 31, 40, 255};
+    Color green = {47, 230, 23, 255};
+    Color red = {232, 18, 18, 255};
+    Color orange = {226, 116, 17, 255};
+    Color yellow = {237, 234, 4, 255};
+    Color purple = {166, 0, 247, 255};
+    Color cyan = {21, 204, 209, 255};
+    Color blue = {13, 64, 216, 255};
 }
