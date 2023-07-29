ecc062a4e8532b0e886e4a5b899a06fee914c9df
maheem_20 <mwistanbuly@gmail.com>
2023-07-29 23:56:38 +0200

    create IBlock class

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -26,3 +26,16 @@ public:
         cells[3] = {Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)};
     }
 };
+
+class IBlock : public Block
+{
+public:
+    IBlock()
+    {
+        id = 3;
+        cells[0] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)};
+        cells[1] = {Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)};
+        cells[2] = {Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)};
+        cells[3] = {Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)};
+    }
+};
