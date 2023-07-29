06d573ac69d2e170f800fe41f3d73908db04bc2d
maheem_20 <mwistanbuly@gmail.com>
2023-07-30 00:00:17 +0200

    create SBlock class

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -52,3 +52,16 @@ public:
         cells[3] = {Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)};
     }
 };
+
+class SBlock : public Block
+{
+public:
+    SBlock()
+    {
+        id = 5;
+        cells[0] = {Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)};
+        cells[1] = {Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)};
+        cells[2] = {Position(1, 1), Position(1, 2), Position(2, 0), Position(2, 1)};
+        cells[3] = {Position(0, 0), Position(1, 0), Position(1, 1), Position(2, 1)};
+    }
+};
