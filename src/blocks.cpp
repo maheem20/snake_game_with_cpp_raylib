cc02716df452c79c0a369cabb0851e0a52bcd44c
maheem_20 <mwistanbuly@gmail.com>
2023-07-29 23:54:23 +0200

    create JBlock class

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -13,3 +13,16 @@ public:
         cells[3] = {Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)};
     }
 };
+
+class JBlock : public Block
+{
+public:
+    JBlock()
+    {
+        id = 2;
+        cells[0] = {Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)};
+        cells[1] = {Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)};
+        cells[2] = {Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 2)};
+        cells[3] = {Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)};
+    }
+};
