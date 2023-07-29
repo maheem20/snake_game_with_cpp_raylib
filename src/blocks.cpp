4b6f3a00f7d63c421c0f62b19950d782b41d54b2
maheem_20 <mwistanbuly@gmail.com>
2023-07-30 00:03:27 +0200

    create ZBlock class

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -78,3 +78,16 @@ public:
         cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 1)};
     }
 };
+
+class ZBlock : public Block
+{
+public:
+    ZBlock()
+    {
+        id = 7;
+        cells[0] = {Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)};
+        cells[1] = {Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)};
+        cells[2] = {Position(1, 0), Position(1, 1), Position(2, 1), Position(2, 2)};
+        cells[3] = {Position(0, 1), Position(1, 0), Position(1, 1), Position(2, 0)};
+    }
+};
