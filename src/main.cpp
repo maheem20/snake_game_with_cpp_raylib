8f37a375e5190792377be90f7097a922447baa86
maheem_20 <mwistanbuly@gmail.com>
2023-08-06 23:52:19 +0200

    add if condition to show 'GAME OVER' text only when game ends

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,10 @@ int main()
         ClearBackground(darkBlue);
         DrawTextEx(font, "Score", {365, 15}, 38, 2, WHITE);
         DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
-        DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        if (game.gameOver)
+        {
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        }
         DrawRectangleRounded({320, 55, 170, 60}, 0.3, 6, lightBlue);
         DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
         game.Draw();
