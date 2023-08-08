1252c88dd139994de6d1a46b25741d3b85dea0e6
maheem_20 <mwistanbuly@gmail.com>
2023-08-08 21:51:32 +0200

    canter I & O blocks in Next panel

--- src/game.cpp
+++ src/game.cpp
@@ -32,7 +32,18 @@ void Game::Draw()
 {
     grid.Draw();
     currentBlock.Draw(11, 11);
-    nextBlock.Draw(270, 270);
+    switch (nextBlock.id)
+    {
+    case 3:
+        nextBlock.Draw(255, 290);
+        break;
+    case 4:
+        nextBlock.Draw(255, 280);
+        break;
+    default:
+        nextBlock.Draw(270, 270);
+        break;
+    }
 }
 
 void Game::HandleInput()
